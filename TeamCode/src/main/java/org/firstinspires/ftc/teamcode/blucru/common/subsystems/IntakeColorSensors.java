1aa6ab28b3fd26a9b8bd6701123ed96a220fc05c
kevinndongg <kevin.dong.haonan@gmail.com>
2024-03-18 09:47:38 -0400

    3.17 testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/IntakeColorSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/IntakeColorSensors.java
@@ -10,39 +10,74 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class IntakeColorSensors implements Subsystem{
     RevColorSensorV3 frontSensor, backSensor;
     NormalizedRGBA frontRGBA, backRGBA;
+    double frontR, frontG, frontB;
+    double backR, backG, backB;
     double frontDistance, backDistance;
     double frontLightDetected, backLightDetected;
     boolean reading;
 
     public IntakeColorSensors(HardwareMap hardwareMap) {
         frontSensor = hardwareMap.get(RevColorSensorV3.class, "front color");
-//        backSensor = hardwareMap.get(RevColorSensorV3.class, "back color");
+        backSensor = hardwareMap.get(RevColorSensorV3.class, "back color");
 
         reading = false;
     }
 
     public void init() {
         frontRGBA = frontSensor.getNormalizedColors();
-//        backRGBA = backSensor.getNormalizedColors();
+        backRGBA = backSensor.getNormalizedColors();
+
+
+
         frontDistance = frontSensor.getDistance(DistanceUnit.INCH);
-//        backDistance = backSensor.getDistance(DistanceUnit.INCH);
+        backDistance = backSensor.getDistance(DistanceUnit.INCH);
         frontLightDetected = frontSensor.getLightDetected();
-//        backLightDetected = backSensor.getLightDetected();
+        backLightDetected = backSensor.getLightDetected();
     }
 
     public void read() {
         if(reading) {
+            frontSensor.enableLed(true);
+            backSensor.enableLed(true);
             frontRGBA = frontSensor.getNormalizedColors();
-//            backRGBA = backSensor.getNormalizedColors();
+            backRGBA = backSensor.getNormalizedColors();
+
+            frontR = frontRGBA.red;
+            frontG = frontRGBA.green;
+            frontB = frontRGBA.blue;
+            scaleFrontRGB();
+
+            backR = backRGBA.red;
+            backG = backRGBA.green;
+            backB = backRGBA.blue;
+            scaleBackRGB();
+
             frontDistance = frontSensor.getDistance(DistanceUnit.INCH);
-//            backDistance = backSensor.getDistance(DistanceUnit.INCH);
+            backDistance = backSensor.getDistance(DistanceUnit.INCH);
             frontLightDetected = frontSensor.getLightDetected();
-//            backLightDetected = backSensor.getLightDetected();
+            backLightDetected = backSensor.getLightDetected();
+        } else {
+            frontSensor.enableLed(false);
+            backSensor.enableLed(false);
         }
     }
 
     public void write() {
 
+    }
+
+    public void scaleFrontRGB() {
+        double max = Math.max(Math.max(frontR, frontB), frontG);
+        frontR = frontR * 255.0 / max;
+        frontG = frontG * 255.0 / max;
+        frontB = frontB * 255.0 / max;
+    }
+
+    public void scaleBackRGB() {
+        double max = Math.max(Math.max(backR, backB), backG);
+        backR = backR * 255.0 / max;
+        backG = backG * 255.0 / max;
+        backB = backB * 255.0 / max;
     }
 
     public void startReading() {
@@ -55,11 +90,17 @@ public class IntakeColorSensors implements Subsystem{
 
     public void telemetry(Telemetry telemetry) {
         telemetry.addData("READING: ", reading);
-        telemetry.addData("front color", frontRGBA.toString());
-//        telemetry.addData("back color", backRGBA.toString());
+        telemetry.addData("front R", frontR);
+        telemetry.addData("front G", frontG);
+        telemetry.addData("front B", frontB);
+        telemetry.addData("front A", frontRGBA.alpha);
+        telemetry.addData("back R", backR);
+        telemetry.addData("back G", backG);
+        telemetry.addData("back B", backB);
+        telemetry.addData("back A", backRGBA.alpha);
         telemetry.addData("front distance", frontDistance);
-//        telemetry.addData("back distance", backDistance);
+        telemetry.addData("back distance", backDistance);
         telemetry.addData("front light detected", frontLightDetected);
-//        telemetry.addData("back light detected", backLightDetected);
+        telemetry.addData("back light detected", backLightDetected);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/testopmodes/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/testopmodes/ColorSensorTest.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.blucru.opmode.testopmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.blucru.common.util.BCLinearOpMode;
 
+@TeleOp(name = "color sensor test", group = "test")
 public class ColorSensorTest extends BCLinearOpMode {
     public void periodic() {
     }
@@ -9,9 +12,5 @@ public class ColorSensorTest extends BCLinearOpMode {
     public void initialize() {
         addIntakeColorSensors();
         intakeColorSensors.startReading();
-    }
-
-    public void telemetry() {
-        intakeColorSensors.telemetry(telemetry);
     }
 }
