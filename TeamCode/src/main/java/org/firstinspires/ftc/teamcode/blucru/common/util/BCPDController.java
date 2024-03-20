3a35d8294e9826f5ee780146810f04d1b07d1f52
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-03-20 11:12:54 -0400

    used dot product instaed of math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/util/BCPDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/util/BCPDController.java
@@ -1,13 +1,20 @@
 package org.firstinspires.ftc.teamcode.blucru.common.util;
 
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.arcrobotics.ftclib.controller.PIDController;
 
 public class BCPDController extends PIDController {
+    Vector2d k;
     public BCPDController(double kP, double kD) {
         super(kP, 0, kD);
+        k = new Vector2d(kP, kD);
     }
 
     public double calculate(double currentPos, double targetPos, double currentVelocity, double targetVelocity) {
-        return getP() * (targetPos - currentPos) + getD() * (targetVelocity - currentVelocity);
+        Vector2d p = new Vector2d(currentPos, currentVelocity);
+        Vector2d target = new Vector2d(targetPos, targetVelocity);
+        Vector2d error = target.minus(p);
+
+        return error.dot(k);
     }
 }
