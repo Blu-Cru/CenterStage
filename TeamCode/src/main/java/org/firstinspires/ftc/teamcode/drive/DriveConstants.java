4501d6f9b4a90fb329e635a3c436a836178c9684
kevinndongg <kevin.dong.haonan@gmail.com>
2024-03-22 08:16:09 -0400

    slowed down auto to match before

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -67,7 +67,7 @@ public class DriveConstants {
      * inches.
      */
     public static double MAX_VEL = 35;
-    public static double MAX_ACCEL = 50;
+    public static double MAX_ACCEL = 40;
     public static double MAX_ANG_VEL = Math.toRadians(200);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
