32dbf63da06234ef2042c8dd90b8bde04cf0c4c3
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-04-11 21:41:18 -0400

    outtake extend command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/commandbase/systemcommand/OuttakeExtendCommand.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.blucru.common.commandbase.systemcommand;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.blucru.common.commandbase.subsystemcommand.OuttakeWristCommand;
+import org.firstinspires.ftc.teamcode.blucru.common.subsystems.Robot;
+
+public class OuttakeExtendCommand extends SequentialCommandGroup {
+    public OuttakeExtendCommand(double pixelHeight) {
+        super(
+                new SequentialCommandGroup(
+                        new InstantCommand(
+                                () -> {
+                                    Robot.getInstance().outtake.setTargetPixelHeight(pixelHeight);
+                                }),
+                        new WaitCommand(500),
+                        new OuttakeWristCommand(false)
+                )
+        );
+    }
+}
