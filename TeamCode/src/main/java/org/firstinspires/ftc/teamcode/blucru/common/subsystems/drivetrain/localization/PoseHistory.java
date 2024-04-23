12cf2999547806c56838759e38e3374d5db55cb9
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-04-23 18:50:51 -0400

    made method to get pose at time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/drivetrain/localization/PoseHistory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/common/subsystems/drivetrain/localization/PoseHistory.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.blucru.common.subsystems.drivetrain.localization;
 
+import android.util.Log;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 import org.firstinspires.ftc.teamcode.blucru.common.subsystems.Robot;
@@ -7,7 +9,7 @@ import org.firstinspires.ftc.teamcode.blucru.common.subsystems.Robot;
 import java.util.ArrayList;
 
 public class PoseHistory {
-    static double STORAGE_NANOSECONDS = 2.0 * Math.pow(10.0, 9.0);
+    static double STORAGE_NANOSECONDS = 1.0 * Math.pow(10.0, 9.0);
 
     static class PoseMarker {
         double nanoTime;
@@ -21,7 +23,14 @@ public class PoseHistory {
 
     ArrayList<PoseMarker> poseHistory;
 
+    /*
+        list of poses and their timestamps
+        poses are stored in the order they were added, so new poses will be at the end of the list
+        timestamps are in nanoseconds
+     */
+
     public PoseHistory() {
+        // initialize pose history
         poseHistory = new ArrayList<>();
     }
 
@@ -29,8 +38,20 @@ public class PoseHistory {
         poseHistory.add(new PoseMarker());
 
         double currentTime = System.nanoTime();
+
+        // remove old poses
         while (poseHistory.size() > 0 && currentTime - poseHistory.get(0).nanoTime > STORAGE_NANOSECONDS) {
             poseHistory.remove(0);
         }
+    }
+
+    public Pose2d getPoseAtTime(double targetNanoTime) {
+        for (int i = poseHistory.size() - 1; i >= 0; i--) {
+            if (poseHistory.get(i).nanoTime < targetNanoTime) {
+                return poseHistory.get(i).pose;
+            }
+        }
+        Log.e("PoseHistory", "No pose found at time " + targetNanoTime);
+        return null;
     }
 }
