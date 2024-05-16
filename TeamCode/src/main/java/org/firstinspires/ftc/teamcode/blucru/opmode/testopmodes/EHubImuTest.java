6f2051aaffc28ec22454b77ec46f787c95222cd2
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-05-15 23:39:10 -0400

    clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/testopmodes/EHubImuTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blucru/opmode/testopmodes/EHubImuTest.java
@@ -19,36 +19,24 @@ public class EHubImuTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-//        Drivetrain dt;
 
         IMU imu = hardwareMap.get(IMU.class, "e hub imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         imu.initialize(parameters);
-//        dt = new Drivetrain(hardwareMap, true);
-//        dt.init();
-//        dt.fieldCentric = false;
         waitForStart();
         while (opModeIsActive()) {
-//            dt.read();
-//            double horz = gamepad1.left_stick_x;
-//            double vert = -gamepad1.left_stick_y;
-//            double rot = -gamepad1.right_stick_y;
-//
-//            dt.teleOpDrive(horz, vert, rot);
 
             if(gamepad1.right_stick_button) {
                 imu.resetDeviceConfigurationForOpMode();
                 imu.resetYaw();
             }
 
-
             YawPitchRollAngles ypr = imu.getRobotYawPitchRollAngles();
             telemetry.addData("Yaw", ypr.getYaw(AngleUnit.DEGREES));
             telemetry.addData("Pitch", ypr.getPitch(AngleUnit.DEGREES));
             telemetry.addData("Roll", ypr.getRoll(AngleUnit.DEGREES));
             telemetry.update();
-//            dt.write();
         }
     }
 }
