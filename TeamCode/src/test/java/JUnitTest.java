027a9145b1322cb24a8d5a3309ef6f569bf078ef
Kevin Dong <107311558+kevinndongg@users.noreply.github.com>
2024-05-18 19:55:11 -0400

    junit test works

---
+++ TeamCode/src/test/java/JUnitTest.java
@@ -0,0 +1,10 @@
+import org.firstinspires.ftc.robotcore.internal.system.Assert;
+
+import org.junit.*;
+
+public class JUnitTest {
+    @Test
+    public void testCar() {
+        Assert.assertEquals(1, 0);
+    }
+}
